26b2a38b489269906e633f7e3fb8478862d4f049
Sviatoslav <55582196+fayonn@users.noreply.github.com>
2020-11-04 03:59:01 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from wsgiref.simple_server import make_server
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/api/v1/hello-world-8')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/')
+def main():
+    return '<h1>Main<h1>'
+
+with make_server('', 8080, app) as server:
+    print("Main http://127.0.0.1:8080")
+    print("Hello World! http://127.0.0.1:8080/api/v1/hello-world-8")
+    server.serve_forever()
+
+if __name__ == '__main__':
+    app.run()
