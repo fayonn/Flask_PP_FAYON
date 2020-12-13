23a0d6bbd4117315be17969181d9ac15c55fa033
Sviatoslav <55582196+fayonn@users.noreply.github.com>
2020-12-13 20:36:06 +0200

    laba 7

--- models.py
+++ models.py
@@ -3,30 +3,29 @@ from migrate import db
 Base = db.Model
 
 
-class Category(db.Model):
+class Category(db.Model):  # не закрив ticket_controller
     __tablename__ = 'categories'
     id = db.Column('id', db.Integer, primary_key=True)
     name = db.Column('name', db.VARCHAR(length=40))
 
 
-class User(db.Model):
+class User(db.Model):  # закрив user_controller
     __tablename__ = 'users'
     id = db.Column('id', db.Integer, primary_key=True)
     username = db.Column('username', db.VARCHAR(length=40), nullable=False)
-    password = db.Column('password', db.VARCHAR(length=40), nullable=False)
+    email = db.Column('email', db.VARCHAR(30), nullable=False)
+    password = db.Column('password', db.VARCHAR(length=120), nullable=False)
 
 
-class Ticket(db.Model):
+class Ticket(db.Model):  # не закрив ticket_controller
     __tablename__ = 'tickets'
     id = db.Column('id', db.Integer, primary_key=True, unique=True)
-    category_id = db.Column(db.Integer, db.ForeignKey(Category.id))
-    category = db.relationship('Category', backref=db.backref("category"))
     name = db.Column('name', db.VARCHAR(length=40), nullable=False)
     description = db.Column('description', db.VARCHAR(length=400))
     price = db.Column('price', db.Integer)
     endTimeReservation = db.Column('endTimeReservation', db.TIMESTAMP)
     reservationPrice = db.Column('reservationPrice', db.Integer)
-    status = db.Enum('available', 'pending', 'sold', 'reserved')
+    status = db.Column('status', db.VARCHAR(20))
 
 
 class Order(db.Model):
@@ -34,8 +33,7 @@ class Order(db.Model):
     id = db.Column('id', db.Integer, primary_key=True, unique=True)
     ticket_id = db.Column('ticket_id', db.Integer, db.ForeignKey(Ticket.id), unique=True)
     ticket = db.relationship("Ticket", backref="order")
-    status = db.Enum('placed', 'payed')
-    complete = db.Column('complete', db.Boolean)
+    status = db.Column('status', db.VARCHAR(20))
 
 
 class Reservation(db.Model):
@@ -43,13 +41,5 @@ class Reservation(db.Model):
     id = db.Column('id', db.Integer, primary_key=True, unique=True)
     ticket_id = db.Column('ticket_id', db.Integer, db.ForeignKey(Ticket.id), unique=True)
     ticket = db.relationship("Ticket", backref="reservation")
-    status = db.Enum('waiting', 'payed')
-    complete = db.Column('complete', db.Boolean)
-
-
-class ApiResponse(db.Model):
-    __tablename__ = 'apiResponse'
-    id = db.Column('id', db.Integer, primary_key=True)
-    type = db.Column('type', db.VARCHAR(length=40))
-    message = db.Column('message', db.VARCHAR(length=40))
+    status = db.Column('status', db.VARCHAR(20))
 
--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 from models import *
 
-category = Category(name='topClass')
-ticket = Ticket(name='theaterTicket', category=category)
-reservation = Reservation(status='waiting', ticket=ticket, complete=False)
-order = Order(status='placed', ticket=ticket, complete=False)
-user = User(username='user', password='password')
+# category = Category(name='topClass')
+# ticket = Ticket(name='theaterTicket', category=category)
+# reservation = Reservation(status='waiting', ticket=ticket, complete=False)
+# order = Order(status='placed', ticket=ticket, complete=False)
+# user = User(username='user', password='password')
 
-# db.create_all()
+db.create_all()
 
-db.session.add(reservation)
-db.session.add(ticket)
-db.session.add(category)
-db.session.add(user)
-db.session.commit()
+# db.session.add(reservation)
+# db.session.add(ticket)
+# db.session.add(category)
+# db.session.add(user)
+# db.session.commit()
